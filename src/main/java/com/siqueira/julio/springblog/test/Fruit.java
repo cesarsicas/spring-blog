2cbffb13af3ed81012b950e45c4fd8a6de7016f9
Julio Siqueira <jctsiqueira@gmail.com>
2019-05-30 15:47:49 -0300

    first commit, crated project and config DB access

---
+++ src/main/java/com/siqueira/julio/springblog/test/Fruit.java
@@ -0,0 +1,24 @@
+package com.siqueira.julio.springblog.test;
+
+public class Fruit {
+    String name;
+    String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+}
---
+++ src/main/java/com/siqueira/julio/springblog/test/TestController.java
@@ -0,0 +1,20 @@
+package com.siqueira.julio.springblog.test;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+
+
+    @GetMapping("/")
+    public ResponseEntity<Fruit> home(){
+
+        Fruit fruit = new Fruit();
+
+        return new ResponseEntity<Fruit>(fruit, HttpStatus.OK);
+    }
+}
